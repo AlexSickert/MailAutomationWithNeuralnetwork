63ff033aac52dcddac78a6adb42b66b731e452ee
admin <admin@DESKTOP-G21D70R>
2016-11-20 12:46:29 +0100

    various improvements

--- src/brainextensionmailpull/ImapStratoJunk.java
+++ src/brainextensionmailpull/ImapStratoJunk.java
@@ -23,21 +23,21 @@ public class ImapStratoJunk {
         System.out.println("email = " + email);
         System.out.println("pass = " + pass);
 
-        IMAPFolder folder = null;
-        Folder folder2 = null;
+//        IMAPFolder folder = null;
+//        Folder folder2 = null;
 
         Folder[] folders = null;
         Folder[] subFolders = null;
 
         Store store = null;
 
-        long timeNow;
-        long timeLast;
-        long messageCounter;
-        long timeDiff;
-        long timeTotal;
-
-        int lastNumInt = 0;
+//        long timeNow;
+//        long timeLast;
+//        long messageCounter;
+//        long timeDiff;
+//        long timeTotal;
+//
+//        int lastNumInt = 0;
 
         try {
             Properties props = System.getProperties();
@@ -58,7 +58,7 @@ public class ImapStratoJunk {
             System.out.println("done ... ");
             System.out.println("opening folder ... ");
 
-            IMAPFolder[] f = (IMAPFolder[]) store.getDefaultFolder().list();
+//            IMAPFolder[] f = (IMAPFolder[]) store.getDefaultFolder().list();
 
             IMAPFolder foTarget = (IMAPFolder) store.getFolder("INBOX.ROBOJUNK");
             foTarget.open(Folder.READ_WRITE);
@@ -72,16 +72,17 @@ public class ImapStratoJunk {
 
             MailSorter sort = new MailSorter();
             sort.testSort(foSource, foTarget);
+            
+            System.out.println("folder processed, now we close the folder and clean them up");
 
             foTarget.close(true);
             foSource.close(true);
+            
+            System.out.println("all done in loopThroughFolders"); 
 
         } catch (Exception e) {
-            
+            System.out.println("error in ImapStratoJunk  loopThroughFolders(String email, String pass)");
             e.printStackTrace();
-
         }
-
     }
-
 }
